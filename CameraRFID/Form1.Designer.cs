d6edb65f61b27dbd6eb84ab3d5b530d575b07bbf
seollove79 <seollove@gmail.com>
2023-06-17 21:06:57 +0900

    backup

---
+++ CameraRFID/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace CameraRFID
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSnapshot = new System.Windows.Forms.Button();
+            this.comboDevices = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(642, 768);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(800, 16);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(800, 46);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(99, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSnapshot
+            // 
+            this.btnSnapshot.Location = new System.Drawing.Point(800, 76);
+            this.btnSnapshot.Name = "btnSnapshot";
+            this.btnSnapshot.Size = new System.Drawing.Size(99, 23);
+            this.btnSnapshot.TabIndex = 3;
+            this.btnSnapshot.Text = "SNAPSHOT";
+            this.btnSnapshot.UseVisualStyleBackColor = true;
+            this.btnSnapshot.Click += new System.EventHandler(this.btnSnapshot_Click);
+            // 
+            // comboDevices
+            // 
+            this.comboDevices.FormattingEnabled = true;
+            this.comboDevices.Location = new System.Drawing.Point(800, 106);
+            this.comboDevices.Name = "comboDevices";
+            this.comboDevices.Size = new System.Drawing.Size(99, 20);
+            this.comboDevices.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.comboDevices);
+            this.Controls.Add(this.btnSnapshot);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSnapshot;
+        private System.Windows.Forms.ComboBox comboDevices;
+    }
+}
+
---
+++ CameraRFID/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace CameraRFID
+{
+    public partial class MainForm : Form
+    {
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                comboDevices.Items.Add(Device.Name);
+            }
+            comboDevices.SelectedIndex = 0; // default to first camera
+            FinalFrame = new VideoCaptureDevice();
+            streamStart();
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboDevices.SelectedIndex].MonikerString);
+
+            if (FinalFrame.VideoCapabilities.Length > 0)
+            {
+                // Set preferred frame size (resolution) to FHD (1920x1080)
+                foreach (VideoCapabilities capability in FinalFrame.VideoCapabilities)
+                {
+                    if (capability.FrameSize.Width == 1920 && capability.FrameSize.Height == 1080)
+                    {
+                        FinalFrame.VideoResolution = capability;
+                        break;
+                    }
+                }
+            }
+
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+        }
+
+        private void streamStart()
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[0].Name);
+
+            if (FinalFrame.VideoCapabilities.Length > 0)
+            {
+                // Set preferred frame size (resolution) to FHD (1920x1080)
+                foreach (VideoCapabilities capability in FinalFrame.VideoCapabilities)
+                {
+                    if (capability.FrameSize.Width == 1920 && capability.FrameSize.Height == 1080)
+                    {
+                        FinalFrame.VideoResolution = capability;
+                        break;
+                    }
+                }
+            }
+
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+        }
+
+        void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image.Dispose();
+            }
+            try
+            {
+                pictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning == true) FinalFrame.SignalToStop();
+        }
+
+        private void btnSnapshot_Click(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning == true)
+            {
+                string filePath = "Snapshot.bmp";
+                if (File.Exists(filePath)) // 파일이 존재하는 경우 memory leak이 발생한다.
+                {
+                    // 파일을 삭제하여 memory leak을 방지한다.
+                    File.Delete(filePath);
+                }
+
+                // 이미지를 저장할 경로와 형식을 설정합니다.
+                ImageFormat format = ImageFormat.Bmp; // 이미지 형식은 PNG로 설정합니다.
+                ImageCodecInfo encoder = GetEncoder(format); // ImageCodecInfo 객체를 가져옵니다.
+                EncoderParameters encoderParams = GetEncoderParameters(format); // EncoderParameter 객체 배열을 가져옵니다.
+
+
+                pictureBox.Image.Save("Snapshot.bmp", encoder, encoderParams);
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+
+            return null;
+        }
+
+        private EncoderParameters GetEncoderParameters(ImageFormat format)
+        {
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 50L); // 이미지 품질 설정
+
+            return encoderParams;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalFrame.IsRunning == true) FinalFrame.SignalToStop();
+        }
+    }
+}
